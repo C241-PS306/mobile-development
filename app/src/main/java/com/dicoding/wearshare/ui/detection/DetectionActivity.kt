ac0812a59babcb8276ce5168466f90c261e8d2fc
najlaput16 <najlaputriafifah16@gmail.com>
2024-06-05 12:16:32 +0700

    add detection and listpanti

---
+++ app/src/main/java/com/dicoding/wearshare/ui/detection/DetectionActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.wearshare.ui.detection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.wearshare.R
+
+class DetectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detection)
+    }
+}
---
+++ app/src/main/java/com/dicoding/wearshare/ui/detection/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.wearshare.ui.detection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.wearshare.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
