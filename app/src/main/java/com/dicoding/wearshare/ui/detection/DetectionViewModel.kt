3c06bcc8490cc47c01ea33044394ace6b94ba848
najlaput16 <najlaputriafifah16@gmail.com>
2024-06-05 12:58:59 +0700

    repair detection and result

--- app/src/main/java/com/dicoding/wearshare/ui/detection/DetectionViewModel.kt
+++ app/src/main/java/com/dicoding/wearshare/ui/detection/DetectionViewModel.kt
@@ -1,13 +1,13 @@
-package com.dicoding.wearshare.ui.notifications
+package com.dicoding.wearshare.ui.detection
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class DetectionViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
